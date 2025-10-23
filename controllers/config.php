bab39fb6d748b5d7c3cafbd4ae78e909bddd4dff
= <victorpianwi@gmail.com>
2025-10-23 19:35:08 +0100

    Updated config file

--- controllers/config.php
+++ controllers/config.php
@@ -9,10 +9,10 @@ if($_SERVER["HTTP_HOST"] == "localhost"){
 
 } else {
 
-    $host = "localhost";
-    $username = "u355950_worldchainltduser";
-    $password = "Fl8@GTQWYbbb";
-    $db = "u355950_worldchainltd";
+    $host = "mysql.railway.internal";
+    $username = "root";
+    $password = "OBuRwWSGGKKUFGvbcgANJxBUaaJzIoxL";
+    $db = "railway";
 
 }
 
